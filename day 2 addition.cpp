d4ea6178b5664c02c8c00f3456fb774aa213aea0
Shweta Singh <shwetasingh5420@gmail.com>
2026-07-04 11:08:09 +0530

    Create day 2 addition.cpp

---
+++ day 2 addition.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"enter any first number =";
+    cin>>a;
+    cout<<"enter any second number =";
+    cin>>b;
+    cout<<"sum:-"<<a+b<<endl;
+    return 0;
